fabe6e85a6ad6188f6b494d21861b01437d00737
jlara <jlaramintic@gmail.com>
2024-12-27 12:34:38 -0500

    Update evoluciones

--- src/controllers/evoluciones.controller.js
+++ src/controllers/evoluciones.controller.js
@@ -13,9 +13,10 @@ export const getEvoluciones = async (req, res) => {
 export const getEvolucion = async (req, res) => {
   try {
     const { id } = req.params;
-    const [rows] = await pool.query("SELECT evoluciones.*,firmas.url FROM evoluciones LEFT JOIN firmas on evoluciones.id=firmas.message WHERE documento = ?", [
-      id,
-    ]);
+    const [rows] = await pool.query(
+      "SELECT evoluciones.*,firmas.url FROM evoluciones LEFT JOIN firmas on evoluciones.id=firmas.message WHERE documento = ?",
+      [id]
+    );
 
     if (rows.length <= 0) {
       return res.status(404).json({ message: "Historia not found" });
@@ -26,7 +27,6 @@ export const getEvolucion = async (req, res) => {
     return res.status(500).json({ message: "Something went wrong" });
   }
 };
-
 
 export const deleteEvoluciones = async (req, res) => {
   try {
@@ -45,26 +45,11 @@ export const deleteEvoluciones = async (req, res) => {
   }
 };
 
-export const createEvolucion = async (req, res) => { try {
-  const { documento, nombre, motivo, sesion, procedimiento, evolucion,firma, odontograma, consentimiento_uno,consentimiento_dos,consentimiento_tres,consentimiento_cuatro,anestesia,codigo_diagnostico,codigo_procedimiento, fecha,hora,fecha_registro } = req.body;
-  const [result] = await pool.query(
-      "INSERT INTO evoluciones (documento, nombre, motivo, sesion, procedimiento, evolucion,firma, odontograma, consentimiento_uno,consentimiento_dos,consentimiento_tres,consentimiento_cuatro,anestesia,codigo_diagnostico,codigo_procedimiento,fecha,hora,fecha_registro) VALUES (?,?, ?, ?, ?, ?, ?, ?, ?,?,?, ?, ?, ?, ?, ?)",
-      [documento, nombre, motivo, sesion, procedimiento, evolucion,firma, odontograma, consentimiento_uno,consentimiento_dos,consentimiento_tres,consentimiento_cuatro,anestesia,codigo_diagnostico,codigo_procedimiento, fecha,hora,fecha_registro]
-  );
-
-  res.status(201).json({ id: result.insertId, documento, nombre, motivo, sesion, procedimiento, evolucion,firma, odontograma,consentimiento_uno,consentimiento_dos,consentimiento_tres,consentimiento_cuatro,anestesia,codigo_diagnostico,codigo_procedimiento, fecha,hora,fecha_registro });   
-} catch (error) {
-  res.status(500).json({ message: "Something went wrong" });
-}
-};
-
-export const updateEvolucion = async (req, res) => {
+export const createEvolucion = async (req, res) => {
   try {
-    const { id } = req.params;
     const {
       documento,
       nombre,
-      motivo,
       sesion,
       procedimiento,
       evolucion,
@@ -79,15 +64,84 @@ export const updateEvolucion = async (req, res) => {
       codigo_procedimiento,
       fecha,
       hora,
-      fecha_registro
+      fecha_registro,
     } = req.body;
-
     const [result] = await pool.query(
-      "UPDATE evoluciones SET documento = IFNULL(?, documento), nombre = IFNULL(?, nombre), motivo = IFNULL(?, motivo), sesion = IFNULL(?, sesion), procedimiento = IFNULL(?, procedimiento), evolucion = IFNULL(?, evolucion),firma = IFNULL(?, firma), odontograma = IFNULL(?, odontograma), consentimiento_uno = IFNULL(?, consentimiento_uno), consentimiento_dos = IFNULL(?, consentimiento_dos), consentimiento_tres = IFNULL(?, consentimiento_tres), consentimiento_cuatro = IFNULL(?, consentimiento_cuatro),  anestesia = IFNULL(?, anestesia), codigo_diagnostico = IFNULL(?, codigo_diagnostico),codigo_procedimiento = IFNULL(?, codigo_procedimiento),fecha = IFNULL(?, fecha), hora = IFNULL(?, hora), fecha_registro = IFNULL(?, fecha_registro)   WHERE id = ?",
+      "INSERT INTO evoluciones (documento, nombre, sesion, procedimiento, evolucion,firma, odontograma, consentimiento_uno,consentimiento_dos,consentimiento_tres,consentimiento_cuatro,anestesia,codigo_diagnostico,codigo_procedimiento,fecha,hora,fecha_registro) VALUES (?, ?, ?, ?, ?, ?, ?, ?,?,?, ?, ?, ?, ?, ?, ?, ?)",
       [
         documento,
         nombre,
-        motivo,
+        sesion,
+        procedimiento,
+        evolucion,
+        firma,
+        odontograma,
+        consentimiento_uno,
+        consentimiento_dos,
+        consentimiento_tres,
+        consentimiento_cuatro,
+        anestesia,
+        codigo_diagnostico,
+        codigo_procedimiento,
+        fecha,
+        hora,
+        fecha_registro,
+      ]
+    );
+
+    res.status(201).json({
+      id: result.insertId,
+      documento,
+      nombre,
+      sesion,
+      procedimiento,
+      evolucion,
+      firma,
+      odontograma,
+      consentimiento_uno,
+      consentimiento_dos,
+      consentimiento_tres,
+      consentimiento_cuatro,
+      anestesia,
+      codigo_diagnostico,
+      codigo_procedimiento,
+      fecha,
+      hora,
+      fecha_registro,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export const updateEvolucion = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      documento,
+      nombre,
+      sesion,
+      procedimiento,
+      evolucion,
+      firma,
+      odontograma,
+      consentimiento_uno,
+      consentimiento_dos,
+      consentimiento_tres,
+      consentimiento_cuatro,
+      anestesia,
+      codigo_diagnostico,
+      codigo_procedimiento,
+      fecha,
+      hora,
+      fecha_registro,
+    } = req.body;
+
+    const [result] = await pool.query(
+      "UPDATE evoluciones SET documento = IFNULL(?, documento), nombre = IFNULL(?, nombre), sesion = IFNULL(?, sesion), procedimiento = IFNULL(?, procedimiento), evolucion = IFNULL(?, evolucion),firma = IFNULL(?, firma), odontograma = IFNULL(?, odontograma), consentimiento_uno = IFNULL(?, consentimiento_uno), consentimiento_dos = IFNULL(?, consentimiento_dos), consentimiento_tres = IFNULL(?, consentimiento_tres), consentimiento_cuatro = IFNULL(?, consentimiento_cuatro),  anestesia = IFNULL(?, anestesia), codigo_diagnostico = IFNULL(?, codigo_diagnostico),codigo_procedimiento = IFNULL(?, codigo_procedimiento),fecha = IFNULL(?, fecha), hora = IFNULL(?, hora), fecha_registro = IFNULL(?, fecha_registro)   WHERE id = ?",
+      [
+        documento,
+        nombre,
         sesion,
         procedimiento,
         evolucion,
